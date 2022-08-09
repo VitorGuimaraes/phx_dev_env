8bd9092863d438d44fe0ad7f69a09909b5619fc1
Vitor Guimarães <vitorrguimaraess@gmail.com>
2022-08-09 09:55:53 -0300

    fix bug when change POSTGRES_DB in .env

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ rm -rf .git
 
 mv .env.sample .env
 
-source .env 
+source .env
 
 # Runs container with shell. You should run "mix phx.new your_project_name" on it
 docker compose -f $COMPOSE_FILE -p $PROJECT_NAME run --rm dev sh
@@ -14,6 +14,19 @@ sudo chown -R $USER *
 
 # project folder's name defined when create phoenix project
 project_folder=$(echo */)
+
+# change POSTGRES_DB in env file accordingly with project name
+db_name="${project_folder}_dev"
+db_name="${db_name/\/""}"
+db_name=$(echo "$db_name" | tr '[:upper:]' '[:lower:]')
+sed -i "s/POSTGRES_DB=.*/POSTGRES_DB=$db_name/" .env
+cat .env
+
+# change pool_size in config/dev.exs to 2
+sed -i "s/pool_size: .*/pool_size: 2/" ${project_folder}config/dev.exs
+
+# change ip to local
+sed -i "s/http: .*/http: [ip: {0, 0, 0, 0}, port: 4000],/" ${project_folder}config/dev.exs
 
 # phoenix project dir 
 project_dir="$(pwd)/${project_folder}"
@@ -32,18 +45,6 @@ new_dir="${current_dir}${project_folder}"
 
 # rename current folder with project name 
 mv $(pwd) $new_dir
-
-# change POSTGRES_DB in env file accordingly with project name
-db_name=$(basename $(pwd))
-db_name="${db_name}_DEV"
-db_name=$(echo "$db_name" | tr '[:upper:]' '[:lower:]')
-sed -i "s/POSTGRES_DB=.*/POSTGRES_DB=$db_name/" .env
-
-# change pool_size in config/dev.exs to 2
-sed -i "s/pool_size: .*/pool_size: 2/" config/dev.exs
-
-# change ip to local
-sed -i "s/http: .*/http: [ip: {0, 0, 0, 0}, port: 4000],/" config/dev.exs
 
 # delete this bash script
 rm run.sh
