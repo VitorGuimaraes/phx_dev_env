0e5b4de07d9ed33e3562a9290415ccf517f7074b
Vitor Guimarães <vitorrguimaraess@gmail.com>
2022-11-20 18:22:23 -0300

    Fix sed command to rename POSTGRES_DB in .env

--- run.sh
+++ run.sh
@@ -70,7 +70,7 @@ sed -i "s/pool_size: .*/pool_size: 2/" ${project_folder_name}config/dev.exs
 sed -i "s/http: .*/http: [ip: {0, 0, 0, 0}, port: 4000],/" ${project_folder_name}config/dev.exs
 
 # change db name in .env file
-sed -i "s/POSTGRES_DB=.*/POSTGRES_DB=${project_folder_name}_db/" .env
+sed -i "s/POSTGRES_DB=.*/POSTGRES_DB=\"$project_folder_name_db\"/" .env
 
 # copy phoenix project files to current dir, inclusive hidden files
 sudo cp -r "${project_folder_name}." $(pwd)
